b5b88cd4885530daf0d619a2a38821dd93002f69
EmilPapafrita <emilbaezsalazar@gmail.com>
2023-08-28 14:41:08 -0400

    Changed game display and added two image(PNG) files.
    Added a grass background image and added an apple instead of a square for snake to eat. Also, changed the score parameter to include 4 blocks in order to accommodate for fruit size.

--- snake.py
+++ snake.py
@@ -10,11 +10,9 @@ window_x = 720
 window_y = 480
 
 # defining colors
-black = pygame.Color(0, 0, 0)
 white = pygame.Color(255, 255, 255)
 red = pygame.Color(255, 0, 0)
-green = pygame.Color(0, 255, 0)
-blue = pygame.Color(0, 0, 128)
+blue = pygame.Color(73, 186, 252)
 
 # Initialising pygame
 pygame.init()
@@ -93,6 +91,13 @@ def game_over():
     # quit the program
     quit()
 
+# Load the apple image
+apple_img = pygame.image.load("apple.png")
+apple_img = pygame.transform.scale(apple_img, (20, 20))  # Resize the image
+# Load the grass image
+grass_img = pygame.image.load("grass.png")
+grass_img = pygame.transform.scale(grass_img, (window_x, window_y))
+
 
 # Main Function
 while True:
@@ -135,8 +140,16 @@ while True:
     # if fruits and snakes collide then scores
     # will be incremented by 10
     snake_body.insert(0, list(snake_position))
-    if snake_position[0] == fruit_position[0] and snake_position[1] == fruit_position[1]:
-        score += 10
+    if (
+            snake_position[0] == fruit_position[0] and snake_position[1] == fruit_position[1]
+    ) or (
+            snake_position[0] == fruit_position[0] and snake_position[1] == fruit_position[1] + 10
+    ) or (
+            snake_position[0] == fruit_position[0] + 10 and snake_position[1] == fruit_position[1]
+    ) or (
+            snake_position[0] == fruit_position[0] + 10 and snake_position[1] == fruit_position[1] + 10
+    ):
+        score += 1
         fruit_spawn = False
     else:
         snake_body.pop()
@@ -144,15 +157,14 @@ while True:
     if not fruit_spawn:
         fruit_position = [random.randrange(1, (window_x // 10)) * 10,
                           random.randrange(1, (window_y // 10)) * 10]
+        fruit_spawn = True
 
-    fruit_spawn = True
-    game_window.fill(blue)
+    game_window.blit(grass_img, (0, 0)) #Using grass image as background
 
     for pos in snake_body:
-        pygame.draw.rect(game_window, green,
-                         pygame.Rect(pos[0], pos[1], 10, 10))
-    pygame.draw.rect(game_window, white, pygame.Rect(
-        fruit_position[0], fruit_position[1], 10, 10))
+        pygame.draw.rect(game_window, blue, pygame.Rect(pos[0], pos[1], 10, 10))
+    # Draw the apple image
+    game_window.blit(apple_img, (fruit_position[0], fruit_position[1]))
 
     # Game Over conditions
     if snake_position[0] < 0 or snake_position[0] > window_x - 10:
