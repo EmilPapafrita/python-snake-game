a7c8fe4a4ed24c88e16a18b80d370196520cec5b
EmilPapafrita <emilbaezsalazar@gmail.com>
2023-08-28 23:46:02 -0400

    Changed snake color and gave it stripes, added more fruits.
    Changed the snake color to red and a light yellow so that it has stripes instead of a solid color. Also added two more fruit images, a mango, and orange. Fruit images are in an array and selected at random.

--- snake.py
+++ snake.py
@@ -1,4 +1,3 @@
-# importing libraries
 import pygame
 import time
 import random
@@ -9,10 +8,11 @@ snake_speed = 15
 window_x = 720
 window_y = 480
 
-# defining colors
+# colors
 white = pygame.Color(255, 255, 255)
 red = pygame.Color(255, 0, 0)
-blue = pygame.Color(73, 186, 252)
+redOrange = pygame.Color(100, 33, 29)
+yellow = pygame.Color(255, 255, 191)
 
 # Initialising pygame
 pygame.init()
@@ -46,58 +46,43 @@ score = 0
 
 # displaying Score function
 def show_score(choice, color, font, size):
-    # creating font object score_font
-    score_font = pygame.font.SysFont(font, size)
-
-    # create the display surface object
-    # score_surface
-    score_surface = score_font.render('Score : ' + str(score), True, color)
-
-    # create a rectangular object for the text
-    # surface object
-    score_rect = score_surface.get_rect()
-
-    # displaying text
-    game_window.blit(score_surface, score_rect)
+    score_font = pygame.font.SysFont(font, size)  # creating font object
+    score_surface = score_font.render('Score : ' + str(score), True, color)  # creating display surface object
+    score_rect = score_surface.get_rect()  # creating rectangular object for text
+    game_window.blit(score_surface, score_rect)  # displaying text
 
 
 # game over function
 def game_over():
-    # creating font object my_font
-    my_font = pygame.font.SysFont('times new roman', 50)
+    my_font = pygame.font.SysFont('times new roman', 50)  # creating font object
 
-    # creating a text surface on which text
-    # will be drawn
-    game_over_surface = my_font.render(
-        'Your Score is : ' + str(score), True, red)
+    game_over_surface = my_font.render(                      # creating a text surface
+        'Your Score is : ' + str(score), True, red)  # where text will be drawn
 
-    # create a rectangular object for the text
-    # surface object
-    game_over_rect = game_over_surface.get_rect()
+    game_over_rect = game_over_surface.get_rect() # rectangular object text surface object
 
-    # setting position of the text
-    game_over_rect.midtop = (window_x / 2, window_y / 4)
+    game_over_rect.midtop = (window_x / 2, window_y / 4)  # setting position of the text
 
-    # blit will draw the text on screen
-    game_window.blit(game_over_surface, game_over_rect)
+    game_window.blit(game_over_surface, game_over_rect)  # blit will draw the text on screen
     pygame.display.flip()
 
-    # after 2 seconds we will quit the program
-    time.sleep(2)
+    time.sleep(2)  # after 2 seconds we will quit the program
+    pygame.quit()  # deactivating pygame library
+    quit()  # quit program
 
-    # deactivating pygame library
-    pygame.quit()
+# Load array of multiple fruit images
+fruit_images = [
+    pygame.transform.scale(pygame.image.load("apple.png"), (20, 20)),
+    pygame.transform.scale(pygame.image.load("mango.png"), (20, 20)),
+    pygame.transform.scale(pygame.image.load("orange.png"), (20, 20)),
+]
 
-    # quit the program
-    quit()
-
-# Load the apple image
-apple_img = pygame.image.load("apple.png")
-apple_img = pygame.transform.scale(apple_img, (20, 20))  # Resize the image
 # Load the grass image
 grass_img = pygame.image.load("grass.png")
 grass_img = pygame.transform.scale(grass_img, (window_x, window_y))
 
+# Initialize the fruit image
+fruit_image = pygame.transform.scale(pygame.image.load("apple.png"), (20, 20))
 
 # Main Function
 while True:
@@ -136,9 +121,8 @@ while True:
     if direction == 'RIGHT':
         snake_position[0] += 10
 
-    # Snake body growing mechanism
-    # if fruits and snakes collide then scores
-    # will be incremented by 10
+    # Snake body growing mechanism if fruits and snakes collide then scores will be incremented by 1.
+    # Since image of fruit takes up 4 blocks instead of 1 each conditional is needed to cover all 4
     snake_body.insert(0, list(snake_position))
     if (
             snake_position[0] == fruit_position[0] and snake_position[1] == fruit_position[1]
@@ -155,16 +139,21 @@ while True:
         snake_body.pop()
 
     if not fruit_spawn:
+        fruit_image = random.choice(fruit_images)  # Select a random fruit image
         fruit_position = [random.randrange(1, (window_x // 10)) * 10,
                           random.randrange(1, (window_y // 10)) * 10]
         fruit_spawn = True
 
-    game_window.blit(grass_img, (0, 0)) #Using grass image as background
+    game_window.blit(grass_img, (0, 0))  # Using grass image as background
 
-    for pos in snake_body:
-        pygame.draw.rect(game_window, blue, pygame.Rect(pos[0], pos[1], 10, 10))
-    # Draw the apple image
-    game_window.blit(apple_img, (fruit_position[0], fruit_position[1]))
+    for i, pos in enumerate(snake_body):  # To get the stripes on snake body
+        if i % 2 == 0:
+            pygame.draw.rect(game_window, redOrange, pygame.Rect(pos[0], pos[1], 10, 10))
+        else:
+            pygame.draw.rect(game_window, yellow, pygame.Rect(pos[0], pos[1], 10, 10))
+
+    # Draw the fruit image
+    game_window.blit(fruit_image, (fruit_position[0], fruit_position[1]))
 
     # Game Over conditions
     if snake_position[0] < 0 or snake_position[0] > window_x - 10:
@@ -179,9 +168,7 @@ while True:
 
     # displaying score continuously
     show_score(1, white, 'times new roman', 20)
-
     # Refresh game screen
     pygame.display.update()
-
     # Frame Per Second /Refresh Rate
     fps.tick(snake_speed)
